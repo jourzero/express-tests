ac87c0a015e6a71060c71b6914e510124999adf1
Eric <eric@epmba.lan>
2019-02-17 17:23:57 -0500

    After running Prettier

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const logger  = require('../lib/appLogger.js');
+const logger = require("../lib/appLogger.js");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  logger.debug("Rendering /users")
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+    logger.debug("Rendering /users");
+    res.send("respond with a resource");
 });
 
 module.exports = router;
