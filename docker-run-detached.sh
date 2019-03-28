77908d6193e7c3fe600b3932dac55efec9e62ab2
Eric <pcderic@gmail.com>
2019-03-28 01:24:16 +0000

    minor updates

--- docker-run-detached.sh
+++ docker-run-detached.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/docker run -p 127.0.0.1:4242:4242 -d --rm --name express-tests express-tests 2>&1
+docker run -p 127.0.0.1:4242:4242 -d --rm --name express-tests express-tests 2>&1
