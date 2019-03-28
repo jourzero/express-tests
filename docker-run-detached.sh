21b2697331f23ee366cfa6b148929aac54520a2a
Eric <eric@epmba.lan>
2019-03-27 21:29:18 -0400

    minor update

--- docker-run-detached.sh
+++ docker-run-detached.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-docker run -p 127.0.0.1:4242:4242 -d --rm --name express-tests express-tests 2>&1
+HOST_SHARE="/var/tmp/logs"
+CTR_SHARE="/app/logs"
+mkdir "$HOST_SHARE" 2>/dev/null
+docker run -p "127.0.0.1:4242:4242" -d --rm --mount "type=bind,source=${HOST_SHARE},target=${CTR_SHARE}" --name express-tests express-tests 2>&1
