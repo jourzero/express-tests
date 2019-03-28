61c3fd77ab0491a23fb14c7241989756aafed0f2
Eric <pcderic@gmail.com>
2019-03-28 04:09:12 +0000

    Update docker run script

--- docker-run-detached.sh
+++ docker-run-detached.sh
@@ -2,4 +2,5 @@
 HOST_SHARE="/var/tmp/logs"
 CTR_SHARE="/app/logs"
 mkdir "$HOST_SHARE" 2>/dev/null
+name="${PWD##*/}"
 docker run -p "144.202.27.76:4242:4242" -d --rm --mount "type=bind,source=${HOST_SHARE},target=${CTR_SHARE}" --name "$name" "$name" 2>&1
